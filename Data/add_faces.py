52e29b95de5212877d1ef58e2bd2acc3ecfca374
Nilmoy29 <azmainfayek7@gmail.com>
2023-12-16 06:22:52 +0900

    nil

--- Data/add_faces.py
+++ Data/add_faces.py
@@ -1,12 +1,12 @@
 import cv2
-
+import os
 faces_data =[]
 video = cv2.VideoCapture(0)         #Video capture object
 facesdetect = cv2.CascadeClassifier('/Users/apple/Face recognation/Data/haarcascade_frontalface_default.xml') # accessing faces have change(copy path) in other devices
 count = 0
-nameID = str(input("Enter Your name: ")).lower()
-path = '/Users/apple/Face recognation/Data/images/'+nameID   #path to store data
-isExists = os.path.exists(path)
+nameID = str(input("Enter Your name: ")).lower()             # Asking name from the users
+path = '/Users/apple/Face recognation/Data/images/'+nameID   #path  to store data
+isExists = os.path.exists(path)                              #Check if its available or not
 if isExists:
     print("Name Already Taken")
     nameID = str(input("Enter your name again: "))
@@ -19,13 +19,13 @@ while True:
     faces =facesdetect.detectMultiScale(frame, 1.3,5)           #Detectfaces with thrash holder value
     for (x,y,w,h) in faces:         #Iterating through the coordinate form the face variable 
         count += 1
-        crop_image = frame[y:yh, x=x+w, :]
 
 
-        cv2.imwrite(name, frame[y:y+h,x:x+w])    #cutting tge frame
+        cv2.imwrite(name, frame[y:y+h,x:x+w])    #cutting the frame and read the images
         cv2.recantgle(frame, (x,y),(x+w,y+h), (50,50,255),1)    #Seeing the face with coordinates, color and thickness
+    cv2.imshow("WindowFrame",frame)   #show the frame
     k = cv2.waitKey(1)              #keyboard binding for breakig the loop
-    if k == ord('q') or len(faces_data) ==100:
+    if k == ord('q') or count > 500:
         break
 
 video.release()                     #releasing video
