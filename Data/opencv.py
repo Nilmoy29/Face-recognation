c4674f7b8afbba14a5071680a435768546240a1a
Nilmoy29 <azmainfayek7@gmail.com>
2023-12-16 20:43:39 +0900

    g

--- Data/opencv.py
+++ Data/opencv.py
@@ -1,2 +1,18 @@
 import cv2
-print(cv2.__version__)
+
+#load Camera
+cap = cv2.VideoCapture(1)
+
+while True:
+    #To get Frame after Frame 
+    ret,frame = cap.read()      
+    #To show the Frame
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    # to quit
+    if key == "s":
+        break
+
+cap.release()
+cv2.destroyWindow()
